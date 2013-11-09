780be0cf522ceedf36d7935e338062e63d12176f
David Rey <david.rey.acosta@gmail.com>
2013-11-09 12:41:11 +0100

    Added webkit nightly

---
+++ Casks/webkit-nightly.rb
@@ -0,0 +1,7 @@
+class WebkitNightly < Cask
+  url 'http://builds.nightly.webkit.org/files/trunk/mac/WebKit-SVN-r158804.dmg'
+  homepage 'http://nightly.webkit.org/'
+  version 'r158804'
+  sha1 '986faf1c1c98e610ee4e40dc05dc200999e8fe18'
+  link 'WebKit.app'
+end
