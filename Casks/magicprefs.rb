fe7a4c2f245effb91fcf201768b44e98f99480df
David Rey <david.rey.acosta@gmail.com>
2013-11-09 12:23:58 +0100

    Added my Casks

---
+++ Casks/magicprefs.rb
@@ -0,0 +1,7 @@
+class Magicprefs < Cask
+  url 'http://magicprefs.com/MagicPrefs.app.zip'
+  homepage 'http://magicprefs.com/'
+  version 'lastest'
+  sha1 '98a675e7b3df64c9c2e57966b38ec385f0e668af'
+  link 'MagicPrefs.app'
+end
---
+++ Casks/virtualboxtz.rb
@@ -0,0 +1,8 @@
+class Virtualboxtz < Cask
+  url 'http://download.virtualbox.org/virtualbox/4.2.18/VirtualBox-4.2.18-88780-OSX.dmg'
+  homepage 'https://www.virtualbox.org/'
+  version '4.2.18-88780'
+  sha1 '2a0f6e48783ec85cccd8027684ddc4ed2da23392'
+  install 'VirtualBox.pkg'
+  uninstall :script => 'VirtualBox_Uninstall.tool', :args => %w[--unattended]
+end
