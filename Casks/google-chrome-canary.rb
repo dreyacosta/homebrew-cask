acd211a8ed8c543fcb8bf0c3d57d9fd663b6b2ab
David Rey <david.rey.acosta@gmail.com>
2014-07-23 02:25:30 +0200

    Updated Google Chrome Canary Cask

--- Casks/google-chrome-canary.rb
+++ Casks/google-chrome-canary.rb
@@ -2,6 +2,6 @@ class GoogleChromeCanary < Cask
   url 'https://storage.googleapis.com/chrome-canary/GoogleChromeCanary.dmg'
   homepage 'https://www.google.ca/intl/en/chrome/browser/canary.html'
   version 'latest'
-  no_checksum
+  sha256 '37446bafc3f2f9417cb9bd8f0b91b0f5a6e5fbf9db80d7b7f46b9f94ef04d1c1'
   link 'Google Chrome Canary.app'
 end
